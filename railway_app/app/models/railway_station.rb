56a52ba8576772ba159447ad84bfaaf55ab19a91
michelin_m <mm19022000@gmail.com>
2021-01-20 16:00:16 +0200

    TeachBase Lesson10-11 Railway Web App With Train And Route Resourse

---
+++ railway_app/app/models/railway_station.rb
@@ -0,0 +1,3 @@
+class RailwayStation < ApplicationRecord
+  validates :name, presence: true
+end
---
+++ railway_app/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :trains
+  resources :railway_stations
+  resources :routes
+  get 'welcome/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+end
