e3b6334a392bb77f58da1dcb00b367df9ce5804f
michelin_m <mm19022000@gmail.com>
2021-01-21 16:16:19 +0200

    TeachBase Lesson13 Railway Web App Views

--- railway_app/app/controllers/trains_controller.rb
+++ railway_app/app/controllers/trains_controller.rb
@@ -63,12 +63,12 @@ class TrainsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_train
-      @train = Train.find(params[:id])
-    end
+  def set_train
+    @train = Train.find(params[:id])
+  end
 
     # Only allow a list of trusted parameters through.
-    def train_params
-      params.require(:train).permit(:number)
-    end
+  def train_params
+    params.require(:train).permit(:number, :current_station_id, :route_id)
+  end
 end
